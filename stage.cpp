0ee22b952f7f08b96187ade0837512b256c83c02
takashithiba <k021g1120@g.neec.ac.jp>
2022-10-06 15:02:05 +0900

    stageの曲がり角の角度変更

--- stage.cpp
+++ stage.cpp
@@ -20,15 +20,18 @@ void stage::Initialize()
 	worldTransform2_.Initialize();
 
 	worldTransform_.scale_ = {5.0f,1.0f,50.0f};
-	worldTransform2_.scale_ = { 50.0f,1.0f,5.0f };
+	worldTransform2_.scale_ = { 5.0f,1.0f,50.0f };
+
+	worldTransform2_.rotation_ ={0.0f,25.0f * affine::Deg2Rad,0.0f};
 	
-	worldTransform_.translation_ = {0.0f,-3.0f,0.0f};
-	worldTransform2_.translation_ = {45.0f,-3.0f,45.0f };
+	worldTransform_.translation_ = {0.0f,-3.0f,-30.0f};
+	worldTransform2_.translation_ = {20.0f,-3.0f,50.0f };
 
 	affine::makeMatIdentity(worldTransform_.matWorld_);
 	affine::makeMatIdentity(worldTransform2_.matWorld_);
 	affine::makeMatScale(worldTransform_.matWorld_,worldTransform_.scale_);
 	affine::makeMatScale(worldTransform2_.matWorld_, worldTransform2_.scale_);
+	affine::makeMatRot(worldTransform2_.matWorld_,worldTransform2_.rotation_);
 	affine::makeMatTrans(worldTransform_.matWorld_,worldTransform_.translation_);
 	affine::makeMatTrans(worldTransform2_.matWorld_, worldTransform2_.translation_);
 	worldTransform_.TransferMatrix();
