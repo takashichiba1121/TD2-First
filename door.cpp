e5fa615c913a2511af01b20d0bd7fd36878933ec
ShoShimizu <k021g1379@g.neec.ac.jp>
2022-10-06 15:24:36 +0900

    連打処理追加途中

--- door.cpp
+++ door.cpp
@@ -10,10 +10,39 @@ door::~door()
 
 void door::Initialize()
 {
+	input_.reset(Input::GetInstance());
+	debugText_.reset(DebugText::GetInstance());
+}
+
+void door::Mash()
+{
+	/*if (mashFlag_ == false && mashNum_ == 0)
+	{
+		mashFlag_ = true;
+	}*/
+
+	/*if (mashFlag_ == true)
+	{
+		
+	}*/
+
+	if (input_->PushKey(DIK_SPACE))
+	{
+		mashNum_++;
+	}
+	if (mashNum_ > 10)
+	{
+		mashNum_ == 0;
+	}
+
+	// デバック文字
+	debugText_->SetPos(50, 20);
+	debugText_->GetInstance()->Printf("Player(%d)",mashNum_);
 }
 
 void door::Update()
 {
+	Mash();
 }
 
 void door::Draw()
--- door.h
+++ door.h
@@ -1,4 +1,9 @@
 #pragma once
+
+#include <memory>
+#include "Input.h"
+#include "DebugText.h"
+
 class door
 {
 public:
@@ -21,6 +26,17 @@ public:
 	/// •`‰æ
 	/// </summary>
 	void Draw();
+
+	bool mashFlag_ = false;
+	UINT mashNum_ = 0;
 private:
+
+	/// <summary>
+	/// ˜A‘Åˆ—
+	/// </summary>
+	void Mash();
+
+	std::unique_ptr<Input> input_ = nullptr;
+	std::unique_ptr<DebugText> debugText_ = nullptr;
 };
 
