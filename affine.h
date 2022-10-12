c66a7066875eb48a6824e2f83e5f4f53e9f2a2a3
keita_momiyama <93181795+keita034@users.noreply.github.com>
2022-10-12 23:10:04 +0900

    値がミスってたので修正

--- affine.h
+++ affine.h
@@ -5,8 +5,8 @@
 namespace affine
 {
 	//度からラジアンに変換する定数
-	constexpr float Deg2Rad = 3.14159265f / 180.0f;
-	constexpr float PIHalf = 3.141592654f / 2.0f;
+	constexpr float Deg2Rad = 3.1415926535897931f / 180.0f;
+	constexpr float PIHalf = 3.1415926535897931f / 2.0f;
 
 	//単位行列
 	void makeMatIdentity(Matrix4& matrix);
