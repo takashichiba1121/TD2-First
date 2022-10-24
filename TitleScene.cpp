73f70f0d005d24b09676ae49d0cf773635858552
ShoShimizu <k021g1379@g.neec.ac.jp>
2022-10-24 17:04:41 +0900

    タイトルの位置を微調整

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -9,7 +9,7 @@ void TitleScene::Initialize()
 void TitleScene::Update()
 {
 	sprite_.reset(Sprite::Create(textureHandle_, { 700,120 }));
-	spriteSpace_.reset(Sprite::Create(pushSpaceHandle_, { 490,600 }));
+	spriteSpace_.reset(Sprite::Create(pushSpaceHandle_, { 490,550 }));
 }
 
 void TitleScene::SpriteDraw()
