dcfb2b4d23e6e72c9e4ca2c558d04b06b6e3e6c3
keita_momiyama <93181795+keita034@users.noreply.github.com>
2022-10-25 23:42:29 +0900

    ブロックとクイズ置いた

--- ObjectManager.cpp
+++ ObjectManager.cpp
@@ -50,59 +50,59 @@ void ObjectManager::Reset()
 	std::unique_ptr<Object> object;
 
 	//ç≈èâÇÃìπ	|
-	//object = std::make_unique<Object>();
-	//object->Initialize({ -1.5f, -1.5f, 280.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+	object = std::make_unique<Object>();
+	object->Initialize({ -1.5f, -1.5f, 280.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
 
-	//objects_.push_back(std::move(object));
+	objects_.push_back(std::move(object));
 
-	//object = std::make_unique<Object>();
-	//object->Initialize({ 1.5f, -1.5f, 300.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+	object = std::make_unique<Object>();
+	object->Initialize({ 1.5f, -1.5f, 300.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
 
-	//objects_.push_back(std::move(object));
+	objects_.push_back(std::move(object));
 
-	////ìπÇ”Ç≥Ç¨
-	//for (size_t i = 0; i < 10; i++)
-	//{
-	//	object = std::make_unique<Object>();
-	//	object->Initialize({ 10.0f - i * 2.0f, -1.5f, 400.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+	//ìπÇ”Ç≥Ç¨
+	for (size_t i = 0; i < 10; i++)
+	{
+		object = std::make_unique<Object>();
+		object->Initialize({ 10.0f - i * 2.0f, -1.5f, 400.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
 
-	//	objects_.push_back(std::move(object));
-	//}
+		objects_.push_back(std::move(object));
+	}
 
-	////ìπÇ”Ç≥Ç¨
-	//for (size_t i = 0; i < 10; i++)
-	//{
-	//	object = std::make_unique<Object>();
-	//	object->Initialize({ 10.0f - i * 2.0f, -1.5f, 500.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+	//ìπÇ”Ç≥Ç¨
+	for (size_t i = 0; i < 10; i++)
+	{
+		object = std::make_unique<Object>();
+		object->Initialize({ 10.0f - i * 2.0f, -1.5f, 500.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
 
-	//	objects_.push_back(std::move(object));
-	//}
+		objects_.push_back(std::move(object));
+	}
 
-	////ìÒå¬ñ⁄ÇÃìπ	/
+	//ìÒå¬ñ⁄ÇÃìπ	/
 
-	////óºòeç«Ç¨
+	//óºòeç«Ç¨
 
-	//for (size_t i = 0; i < 3; i++)
-	//{
-	//	for (size_t j = 0; j < 8; j++)
-	//	{
-	//		if (j != 3 && j != 4)
-	//		{
-	//			object = std::make_unique<Object>();
-	//			object->Initialize({ 67.0f - j * 1.75f, 2.5f - 2.0f * i, 850.0f + j * 1.0f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
-	//			objects_.push_back(std::move(object));
-	//		}
-	//	}
-	//}
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 8; j++)
+		{
+			if (j != 3 && j != 4)
+			{
+				object = std::make_unique<Object>();
+				object->Initialize({ 67.0f - j * 1.75f, 2.5f - 2.0f * i, 850.0f + j * 1.0f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
+				objects_.push_back(std::move(object));
+			}
+		}
+	}
 
-	////ìπÇ”Ç≥Ç¨
-	//for (size_t j = 0; j < 9; j++)
-	//{
+	//ìπÇ”Ç≥Ç¨
+	for (size_t j = 0; j < 9; j++)
+	{
 
-	//		object = std::make_unique<Object>();
-	//		object->Initialize({ 298.0f - j * 1.75f, -1.5f , 1250.0f + j * 1.0f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
-	//		objects_.push_back(std::move(object));
-	//}
+			object = std::make_unique<Object>();
+			object->Initialize({ 298.0f - j * 1.75f, -1.5f , 1250.0f + j * 1.0f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
+			objects_.push_back(std::move(object));
+	}
 
 	//3å¬ñ⁄ÇÃìπ
 
@@ -133,5 +133,114 @@ void ObjectManager::Reset()
 		}
 	}
 
+	//4å¬ñ⁄ÇÃìπ
 
+	//êˆÇÈ
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (i != 1 && i != 2)
+		{
+			object = std::make_unique<Object>();
+			object->Initialize({ 750.0f + i * 2.0f,-1.5f,440.3f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+
+			objects_.push_back(std::move(object));
+		}
+		else
+		{
+			object = std::make_unique<Object>();
+			object->Initialize({ 750.0f + i * 2.0f,0.5f,440.3f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+
+			objects_.push_back(std::move(object));
+		}
+
+	}
+
+	//êˆÇÈ
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (i != 1 && i != 2)
+		{
+			object = std::make_unique<Object>();
+			object->Initialize({ 744.0f + i * 2.0f,-1.5f,160.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+
+			objects_.push_back(std::move(object));
+		}
+		else
+		{
+			object = std::make_unique<Object>();
+			object->Initialize({ 744.0f + i * 2.0f,0.5f,160.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+
+			objects_.push_back(std::move(object));
+		}
+
+	}
+
+	//ìπÇ”Ç≥Ç¨
+	for (size_t i = 0; i < 10; i++)
+	{
+		object = std::make_unique<Object>();
+		object->Initialize({ 740.0f + i * 2.0f, -1.5f, 70.0f }, { 0.0f, 0.0f, 0.0f }, 1.0f, Model::Create());
+
+		objects_.push_back(std::move(object));
+	}
+
+	//5å¬ñ⁄ÇÃìπ
+
+	//êˆÇÈ
+	for (size_t i = 0; i < 8; i++)
+	{
+		if (i != 1 && i != 2 && i != 5 && i != 6)
+		{
+			object = std::make_unique<Object>();
+			object->Initialize({ 699.0f - i * 1.75f, -1.5f , -100.0f + i * 1.0f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
+
+			objects_.push_back(std::move(object));
+		}
+		else
+		{
+			object = std::make_unique<Object>();
+			object->Initialize({ 699.0f - i * 1.75f, 0.5f , -100.0f + i * 1.0f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
+
+			objects_.push_back(std::move(object));
+		}
+
+	}
+
+	//ìπÇ”Ç≥Ç¨
+	for (size_t k = 0; k < 3; k++)
+	{
+		for (size_t j = 0; j < 7; j++)
+		{
+			for (size_t i = 0; i < 5; i++)
+			{
+				object = std::make_unique<Object>();
+				object->Initialize({ (586.0f - i * 1.75f) - j * 1.0f, 2.5f - 2.0f * k, (-284.0f + i * 1.0f) - j * 1.75f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
+				objects_.push_back(std::move(object));
+			}
+		}
+	}
+
+	for (size_t k = 0; k < 3; k++)
+	{
+		for (size_t j = 0; j < 7; j++)
+		{
+			for (size_t i = 0; i < 5; i++)
+			{
+				object = std::make_unique<Object>();
+				object->Initialize({ (506.0f - i * 1.75f) - j * 1.0f, 2.5f - 2.0f * k, (-439.0f + i * 1.0f) - j * 1.75f }, { 0.0f, 30.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
+				objects_.push_back(std::move(object));
+			}
+		}
+	}
+
+	//6å¬ñ⁄ÇÃìπ
+
+	//ìπÇ”Ç≥Ç¨
+	for (size_t i = 0; i < 9; i++)
+	{
+		object = std::make_unique<Object>();
+		object->Initialize({ 50.0f + i * 1.75f, -1.5f, -100.0f  + i * 1.0f }, { 0.0f,60.0f * affine::Deg2Rad, 0.0f }, 1.0f, Model::Create());
+
+		objects_.push_back(std::move(object));
+	}
 }
--- SpeedUpChance.cpp
+++ SpeedUpChance.cpp
@@ -4,14 +4,21 @@ void SpeedUpChance::Initialize(RailCamera* railCamera)
 {
 	bodyModel_.reset(Model::CreateFromOBJ("branchPoint"));
 
-	/*std::unique_ptr<BranchPoint> branchPoint;
-	branchPoint = std::make_unique<BranchPoint>();
+	std::unique_ptr<BranchPoint> branchPoint;
 
-	branchPoint->Initialize({ 0.0f,-10.0f,300.0f },{ 0.0f,0.0f,0.0f },{1.0f,1.5f,0.01f},
+	//1‰ñ–Ú
+	branchPoint = std::make_unique<BranchPoint>();
+	branchPoint->Initialize({ 750.0f,-10.0f,520.3f }, { 0.0f,180 * affine::Deg2Rad,0.0f }, { 1.0f,1.5f,0.01f },
 		bodyModel_.get(),
 		std::bind(&RailCamera::SpeedDown, railCamera), std::bind(&RailCamera::SpeedUp, railCamera));
+	branchPoints.push_back(std::move(branchPoint));
 
-	branchPoints.push_back(std::move(branchPoint));*/
+	//2‰ñ–Ú
+	branchPoint = std::make_unique<BranchPoint>();
+	branchPoint->Initialize({ 750.0f,-10.0f,260.3f }, { 0.0f,180 * affine::Deg2Rad,0.0f }, { 1.0f,1.5f,0.01f },
+		bodyModel_.get(),
+		std::bind(&RailCamera::SpeedUp, railCamera), std::bind(&RailCamera::SpeedDown, railCamera));
+	branchPoints.push_back(std::move(branchPoint));
 }
 
 void SpeedUpChance::Update(Player* player)
